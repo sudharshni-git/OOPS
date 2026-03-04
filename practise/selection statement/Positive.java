737742b621c002fe8d7c76c43b72745e5d640806
sudharshni-git <sudharshniraj@gmail.com>
2026-03-04 18:04:56 +0530

    Add files via upload

---
+++ practise/selection statement/Positive.java
@@ -0,0 +1,11 @@
+class Positive {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
