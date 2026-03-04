50af063bed5d60866f81e2e60a114df4688d55f9
sudharshni-git <sudharshniraj@gmail.com>
2026-03-04 18:56:38 +0530

    Add files via upload

---
+++ practise/selection statement/Option.java
@@ -0,0 +1,16 @@
+class Option {
+    public static void main(String[] args) {
+        int choice = 2;
+
+        switch (choice) {
+            case 1:
+                System.out.println("Option One");
+                break;
+            case 2:
+                System.out.println("Option Two");
+                break;
+            default:
+                System.out.println("Invalid Option");
+        }
+    }
+}
